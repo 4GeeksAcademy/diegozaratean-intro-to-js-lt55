c955a99a7fca6fcb1403f996d4242f32d64f480e
diego <diegozaratean@gmail.com>
2025-06-13 23:45:02 +0000

    finis intro js

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log('hoal desde mainjs')
+
+let nombre = 'goku';
+let edad  = 25
+let vivo = false
+// var esposa ='milk'
+const raza = 'sayayin'
+
+// item.          1.    2.     3.       4.     5 
+let frutas = ['mango','uva','manzana','lulo','granada']
+// position.      0.    1.      2.       3.      4
+// length.  5
+
+
+const perro = {color: 'negro', raza:'callejera puddle', nombre:'bombom', edad: 10}
+
+
+
+// superheroe,. los poderes, disfraz, identidad, nombre
+let superheroe = {nombre: 'batman' , poderes: ['dinero' , 'detective', 'habilidad fisica'], disfraz:'murcielago', identidad: 'bruce wayne'} 
+
+
